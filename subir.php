ee7346753f5f7a4bb64d9091660dc83f976df780
AlberttMora <moraAlbertt13092022@gmail.com>
2026-09-10 10:30:41 -0600

    subir

--- subir.php
+++ subir.php
@@ -1,6 +1,18 @@
 <?php
 
-$data = $_POST;
+include("parts/css.php");
 
-$detalles = json_decode($data[''], true);
+$data = $_POST;
+$archivo = $_FILES['archivo'];
+
+    if(file_exists($archivo)) {
+        $ruta = "uploads/" . $archivo['name'];
+        move_uploaded_file($archivo['tmp_name'], $ruta);
+        echo "Archivo subido correctamente a la ruta: " . $ruta;
+    } else {
+        echo "<p p-2>Error al subir el archivo</p>". $archivo["name"] ."";
+        echo '<a href="index.php" class="btn btn-primary">Regresar al formulario</a>';
+    }
+
+    ?>
 
