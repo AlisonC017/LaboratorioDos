357d3b04a9f3a48d97e36721bd3433da76fc4ce1
ruth-shb <ruthsarayh@gmail.com>
2026-09-10 10:58:15 -0600

    e

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <?php include("partes/css.php"); ?>
+    <?php include("parts/css.php"); ?>
 
     <style>
         body {
