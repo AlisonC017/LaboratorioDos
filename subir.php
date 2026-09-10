ac0492d992a8c9ece7e660bcb23a041128b6eda0
AlberttMora <moraAlbertt13092022@gmail.com>
2026-09-10 11:06:53 -0600

    espacio

--- subir.php
+++ subir.php
@@ -8,7 +8,7 @@ $archivo = $_FILES['archivo'];
     if($archivo['error'] === UPLOAD_ERR_OK) {
         $ruta = "uploads/" . $archivo['name'];
         move_uploaded_file($archivo['tmp_name'], $ruta);
-        echo "Archivo subido correctamente a la ruta: " . $ruta;
+        echo "<p p-2> Archivo subido correctamente a la ruta: " . $ruta . "</p>";
     } else {
         echo "<p p-2>Error al subir el archivo". $archivo["name"] ."</p>";
         echo '<a href="index.php" class="btn btn-primary">Regresar al formulario</a>';
