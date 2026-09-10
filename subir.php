653921d24a4633b0af8b2e0623bcc975e7d6bfef
AlberttMora <moraAlbertt13092022@gmail.com>
2026-09-10 11:05:46 -0600

    error

--- subir.php
+++ subir.php
@@ -5,12 +5,12 @@ include("parts/css.php");
 $data = $_POST;
 $archivo = $_FILES['archivo'];
 
-    if(file_exists($archivo)) {
+    if($archivo['error'] === UPLOAD_ERR_OK) {
         $ruta = "uploads/" . $archivo['name'];
         move_uploaded_file($archivo['tmp_name'], $ruta);
         echo "Archivo subido correctamente a la ruta: " . $ruta;
     } else {
-        echo "<p p-2>Error al subir el archivo</p>". $archivo["name"] ."";
+        echo "<p p-2>Error al subir el archivo". $archivo["name"] ."</p>";
         echo '<a href="index.php" class="btn btn-primary">Regresar al formulario</a>';
     }
 
